ab679d8f944f0b6a86d5c4f47a3cf0c7e3ebad72
zkrxnp <102350084+zkrxnp@users.noreply.github.com>
2025-12-09 15:03:51 +0900

    Update vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,11 +1,11 @@
-####################vpc####################
+####################vpc create####################
 resource "aws_vpc" "iac-vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
 
-####################서브넷####################
+####################subnet create####################
 resource "aws_subnet" "iac-public-subnet1"{
   vpc_id = aws_vpc.iac-vpc.id
   cidr_block = "10.0.5.0/24"
@@ -30,12 +30,12 @@ resource "aws_subnet" "iac-private-subnet2"{
   availability_zone = var.region_c
 }
 
-####################인터넷 게이트웨이####################
+####################internet gateway create####################
 resource "aws_internet_gateway" "iac-igw"{
   vpc_id = aws_vpc.iac-vpc.id
 }
 
-####################eip 설정####################
+####################eip create####################
 resource "aws_eip" "iac-eip-a"{
 
 }
@@ -44,7 +44,7 @@ resource "aws_eip" "iac-eip-c"{
 
 }
 
-####################nat 게이트웨이####################
+####################nat gateway create####################
 resource "aws_nat_gateway" "iac_nat_gateway1" {
   allocation_id = aws_eip.iac-eip-a.id
   subnet_id = aws_subnet.iac-public-subnet1.id
@@ -55,7 +55,7 @@ resource "aws_nat_gateway" "iac_nat_gateway2" {
   subnet_id = aws_subnet.iac-public-subnet2.id
 }
 
-####################라우팅 테이블####################
+####################route table create####################
 resource "aws_route_table" "iac-private1-route-table"{
   vpc_id = aws_vpc.iac-vpc.id
 }
@@ -68,7 +68,7 @@ resource "aws_route_table" "iac-public-route-table"{
   vpc_id = aws_vpc.iac-vpc.id
 }
 
-####################라우팅 테이블 - 서브넷 연결####################
+####################route table and subnet connecting####################
 resource "aws_route_table_association" "iac-attach-route-table-subnet1" {
   subnet_id      = aws_subnet.iac-public-subnet1.id
   route_table_id = aws_route_table.iac-public-route-table.id
@@ -89,7 +89,7 @@ resource "aws_route_table_association" "iac-attach-route-table-subnet4" {
   route_table_id = aws_route_table.iac-private2-route-table.id
 }
 
-####################라우팅 규칙####################
+####################route rule####################
 resource "aws_route" "iac-route-rule1" {
   route_table_id         = aws_route_table.iac-private1-route-table.id
   destination_cidr_block = "0.0.0.0/0"
@@ -106,4 +106,5 @@ resource "aws_route" "iac-route-rule3" {
   route_table_id         = aws_route_table.iac-public-route-table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.iac-igw.id
+
 }
